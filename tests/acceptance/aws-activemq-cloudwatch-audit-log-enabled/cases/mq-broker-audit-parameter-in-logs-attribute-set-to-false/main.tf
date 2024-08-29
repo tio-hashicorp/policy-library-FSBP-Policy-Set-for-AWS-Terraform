38881dddf48ab63f1d0e05addcf318c3ba803f05
Tushar Chaudhary <tushar.chaudhary@tushar.chaudhary-LFG2WD0J59>
2024-08-29 16:58:45 +0530

    Formatted files

--- tests/acceptance/aws-activemq-cloudwatch-audit-log-enabled/cases/mq-broker-audit-parameter-in-logs-attribute-set-to-false/main.tf
+++ tests/acceptance/aws-activemq-cloudwatch-audit-log-enabled/cases/mq-broker-audit-parameter-in-logs-attribute-set-to-false/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      version = " ~> 3.76"
+      source  = "registry.terraform.io/hashicorp/aws"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-west-2"
 }
