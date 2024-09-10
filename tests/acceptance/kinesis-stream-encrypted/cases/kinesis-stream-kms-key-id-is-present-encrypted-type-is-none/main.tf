adab20828cc35ff24bf1024b42556750a7b6dfc5
Tushar Chaudhary <tushar.chaudhary@tushar.chaudhary-LFG2WD0J59>
2024-09-11 02:56:00 +0530

    Comments Resolves

--- tests/acceptance/kinesis-stream-encrypted/cases/kinesis-stream-kms-key-id-is-present-encrypted-type-is-none/main.tf
+++ tests/acceptance/kinesis-stream-encrypted/cases/kinesis-stream-kms-key-id-is-present-encrypted-type-is-none/main.tf
@@ -21,6 +21,6 @@ resource "aws_kinesis_stream" "test_stream" {
   }
 
   kms_key_id = "arn:aws:kms:us-west-2:123456789012:key/abcd1234-a123-456a-a12b-a123b4cd56ef"
-  
+
   encryption_type = "NONE"
 }
