644b60b98c4ea5c7bffc847195536784424f8886
Nagateja2402 <Nagateja590@gmail,com>
2024-09-25 11:20:59 +0530

    Added the redshift-cluster-should-be-encrypted-at-rest policy

---
+++ tests/acceptance/redshift-cluster-should-be-encrypted-at-rest/cases/redshift-cluster-with-encrypted-true-nested/redshift-cluster/main.tf
@@ -0,0 +1,12 @@
+resource "aws_redshift_cluster" "rscluster" {
+  cluster_identifier                  = "tf-redshift-cluster"
+  database_name                       = "newdb"
+  master_username                     = "exampleuser"
+  master_password                     = "Newpass12345678"
+  node_type                           = "dc1.large"
+  cluster_type                        = "single-node"
+  encrypted                           = var.encrypted_variable
+  publicly_accessible                 = true
+  automated_snapshot_retention_period = 10
+  allow_version_upgrade               = false
+}
---
+++ tests/acceptance/redshift-cluster-should-be-encrypted-at-rest/cases/redshift-cluster-with-encrypted-true/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      version = " ~> 3.76"
+      source  = "registry.terraform.io/hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_redshift_cluster" "rscluster" {
+  cluster_identifier                  = "tf-redshift-cluster"
+  database_name                       = "newdb"
+  master_username                     = "exampleuser"
+  master_password                     = "Newpass12345678"
+  node_type                           = "dc1.large"
+  cluster_type                        = "single-node"
+  encrypted                           = true
+  publicly_accessible                 = true
+  automated_snapshot_retention_period = 10
+  allow_version_upgrade               = false
+}
