3498d1aca45c402df0c15ca8f65f2659b79b5e1e
Tushar Chaudhary <tushar.chaudhary@tushar.chaudhary-LFG2WD0J59>
2024-11-26 15:14:56 +0530

    formatted

--- tests/acceptance/cloudfront-distributions-should-have-logging-enabled/cases/logging-config-present/main.tf
+++ tests/acceptance/cloudfront-distributions-should-have-logging-enabled/cases/logging-config-present/main.tf
@@ -21,8 +21,8 @@ locals {
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    domain_name              = aws_s3_bucket.b.bucket_regional_domain_name
-    origin_id                = local.s3_origin_id
+    domain_name = aws_s3_bucket.b.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
   }
 
   enabled             = true
