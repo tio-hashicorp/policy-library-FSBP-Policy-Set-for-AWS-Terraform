d8596a4e1fa27984175698f56a4ed8a77034cd41
Tushar Chaudhary <tushar.chaudhary@tushar.chaudhary-LFG2WD0J59>
2025-01-09 13:58:34 +0530

    Accepentance test

--- tests/acceptance/guardduty-eks-protection-runtime-should-be-enabled/cases/eks-runtime-monitoring-disabled/main.tf
+++ tests/acceptance/guardduty-eks-protection-runtime-should-be-enabled/cases/eks-runtime-monitoring-disabled/main.tf
@@ -9,10 +9,5 @@ resource "aws_guardduty_detector" "example" {
 resource "aws_guardduty_detector_feature" "eks_runtime_monitoring" {
   detector_id = aws_guardduty_detector.example.id
   name        = "EKS_RUNTIME_MONITORING"
-  status      = "ENABLED"
-
-  additional_configuration {
-    name   = "EKS_ADDON_MANAGEMENT"
-    status = "DISABLED"
-  }
+  status      = "DISABLED"
 }
