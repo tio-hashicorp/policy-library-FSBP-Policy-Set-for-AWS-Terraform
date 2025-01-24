5c10cebe49415922e3f9c937e4050388ac0b2b9c
Nagateja2402 <nagateja590@gmail.com>
2025-01-24 15:28:27 +0530

    modified the acceptance test to pass

--- tests/acceptance/opensearch-encrypted-at-rest/cases/opensearch-encrypted-at-rest-enabled-nested/main.tf
+++ tests/acceptance/opensearch-encrypted-at-rest/cases/opensearch-encrypted-at-rest-enabled-nested/main.tf
@@ -2,19 +2,6 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_opensearch_domain" "example" {
-  domain_name    = "example"
-  engine_version = "Elasticsearch_7.10"
-
-  cluster_config {
-    instance_type = "r4.large.search"
-  }
-
-  encrypt_at_rest {
-    enabled = false
-  }
-
-  tags = {
-    Domain = "TestDomain"
-  }
+module "opensearch-resources" {
+  source = "./opensearch-resources"
 }
