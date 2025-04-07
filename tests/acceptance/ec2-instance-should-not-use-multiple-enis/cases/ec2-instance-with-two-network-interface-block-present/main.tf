c174519166fd745716786c695f3c037d74bfdce2
Nagateja2402 <nagateja590@gmail.com>
2025-04-07 10:46:13 +0530

    Modified terraform files for acceptance tests

--- tests/acceptance/ec2-instance-should-not-use-multiple-enis/cases/ec2-instance-with-two-network-interface-block-present/main.tf
+++ tests/acceptance/ec2-instance-should-not-use-multiple-enis/cases/ec2-instance-with-two-network-interface-block-present/main.tf
@@ -2,25 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"]
-
-}
-
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-0655cec52acf2717b"
   instance_type = "t3.micro"
 
   network_interface {
